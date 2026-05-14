011a4f0edab88dcfdca2830da84e9cd91c7e995c
CarolineOliveiraSilva <carolline1995@hotmail.com>
2026-05-14 08:19:46 -0300

    Refactor: removendo arquivos de configuração da IDE (.project)

---
+++ 06-estruturas-de-dados/src/application/NumerosPares.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumerosPares {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros?");
+        int n = sc.nextInt();
+        int[] vect = new int[n];
+        
+        System.out.println("Numeros:");
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextInt();
+        }
+
+        int contador = 0;
+        System.out.println("Esses foram os números pares2:");
+        for (int i = 0; i < n; i++) {
+            if (vect[i] % 2 == 0) {
+                System.out.print(vect[i] + " ");
+                contador++;
+            }
+        }
+        System.out.println("\nTOTAL = " + contador);
+
+        sc.close();
+    }
+}
